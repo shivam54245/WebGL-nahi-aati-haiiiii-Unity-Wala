69c179ad7470fd1030204f6e1a4b41b92d128439
shivam54245 <shivam54245@gmail.com>
2026-03-13 00:00:34 +0530

    hmm

---
+++ Assets/Physics_Gravity.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class Mass : MonoBehaviour
+{
+    public float value = 10f;
+}
+
+
+public class Gravity : MonoBehaviour
+{
+
+    public GameObject targetObject;
+    public float mass = 10f;
+    public Vector2 velocity = new Vector2(0, 0);
+    private float G = 6.626f;
+    private Mass mass_self;
+
+
+    void Start()
+    {
+        mass_self.value = mass;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mass_targetObject_value = targetObject.GetComponent<Mass>().value;
+        Vector2 distance = transform.position - transform.position;
+        Vector2 acceleration = G * mass_targetObject_value * distance.normalized / (distance.magnitude * distance.magnitude);
+
+        velocity += acceleration;
+        transform.Translate(velocity * Time.deltaTime);
+
+    }
+}
