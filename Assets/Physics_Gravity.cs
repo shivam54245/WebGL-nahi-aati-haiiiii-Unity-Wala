3c9430bc110050aa72a8a5622294f814b83c5e92
shivam54245 <shivam54245@gmail.com>
2026-03-20 17:21:38 +0530

    Yeahhh

--- Assets/Physics_Gravity.cs
+++ Assets/Physics_Gravity.cs
@@ -8,6 +8,7 @@ public class Gravity : MonoBehaviour
     public Vector2 velocity = new Vector2(0, 0);
     private float G = 6.626f;
     private Mass mass_self;
+    private float mass_targetObject_value;
 
 
     void Awake()
@@ -17,16 +18,21 @@ public class Gravity : MonoBehaviour
         Debug.Log("Mass of self: " + mass_self.value);
     }
 
+    void Start()
+    {
+        mass_targetObject_value = targetObject.GetComponent<Mass>().value;
+        Vector2 distance = targetObject.transform.position - transform.position;
+        Vector2 acceleration = G * mass_targetObject_value * distance.normalized / distance.sqrMagnitude;
+        velocity += 0.5f * Time.fixedDeltaTime * acceleration;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float mass_targetObject_value = targetObject.GetComponent<Mass>().value;
+        transform.position += (Vector3)(velocity * Time.fixedDeltaTime);
         Vector2 distance = targetObject.transform.position - transform.position;
         Vector2 acceleration = G * mass_targetObject_value * distance.normalized / distance.sqrMagnitude;
-
-        velocity += acceleration*Time.deltaTime;
-        transform.Translate(velocity * Time.deltaTime);
+        velocity += acceleration*Time.fixedDeltaTime;
 
     }
 }
